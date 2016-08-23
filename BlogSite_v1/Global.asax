815d10585b3cdd54bdf3403a0931087c10a6097d
asus <asus@DESKTOP-8PU9BP1>
2016-08-23 11:11:36 +0300

    Add project files.

---
+++ BlogSite_v1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlogSite_v1.MvcApplication" Language="C#" %>
---
+++ BlogSite_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogSite_v1.Startup))]
+namespace BlogSite_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
